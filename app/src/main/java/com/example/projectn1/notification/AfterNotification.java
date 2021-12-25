b65a605316762d14cf871426f37e0ab046b52889
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2021-12-25 18:16:20 +0400

    use notification

--- app/src/main/java/com/example/projectn1/notification/AfterNotification.java
+++ app/src/main/java/com/example/projectn1/notification/AfterNotification.java
@@ -1,10 +1,4 @@
 package com.example.projectn1.notification;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 public class AfterNotification {
-
 }
--- app/src/main/java/com/example/projectn1/notification/NotificationActivity.java
+++ app/src/main/java/com/example/projectn1/notification/NotificationActivity.java
@@ -11,14 +11,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.RemoteViews;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.core.app.NotificationCompat;
 
 import com.example.projectn1.R;
 
@@ -35,7 +33,7 @@ public class NotificationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.before_notification_layout);
+        setContentView(R.layout.notification_layout);
         final RemoteViews[] contentView = new RemoteViews[1];
 
         sendN = findViewById(R.id.send_notification);
@@ -43,10 +41,6 @@ public class NotificationActivity extends AppCompatActivity {
         nM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         sendN.setOnClickListener(v -> {
-           /* Intent intent = new Intent(this, AfterNotification.class);
-            pendingIntent = PendingIntent.getActivity(
-                    this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    contentView[0] = new RemoteViews(getPackageName(), R.layout.afeter_notification_layout);*/
 
             Intent intent = new Intent(this, AfterNotification.class);
 
